381bae158d0968c39a180ec7276a4f817b085c51
DoanTriett <145737710+DoanTriett@users.noreply.github.com>
2025-03-08 22:47:16 +0700

    8/3 update lv2

--- Levels/Level2.py
+++ Levels/Level2.py
@@ -1,6 +1,43 @@
+from EntitiesManager import EntitiesManager as EM
+from Config import Config, Object
+from collections import deque
+import pygame
+
 class Level2:
-  def __init__(self):
-    pass
-  
-  def execute(self):
-    print('Code Here')
+    def __init__(self):
+        pass
+
+    def execute(self):
+        clock = pygame.time.Clock()
+
+        countFrames = 0 # 0
+        #listPos = deque(EM().pinkGhost.pinkGhostForLv2((Object.pinkGhostX, Object.pinkGhostY), (Object.pacmanX, Object.pacmanY)))
+
+
+        while Config.running:
+
+            #Config.screen.fill('black')
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    Config.running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        print("Phím Q được ấn")
+                        Config.running = False
+            
+            if countFrames % 15 == 0:
+                # if listPos:
+                #     newPos = listPos.popleft()
+                #     EM().pinkGhost.updatePosForLv2(newPos)
+                EM().pinkGhost.updatePos()
+            
+            EM().pinkGhost.move()
+
+            EM().maze.draw()
+            EM().pacman.draw()
+            EM().pinkGhost.draw()
+
+            pygame.display.flip()
+            clock.tick(Config.fps)
+            countFrames += 1
